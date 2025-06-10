cea46c0499cdd976a6373949ba632cfebcbc2897
Omer Yilmaz <92752641+TsWty@users.noreply.github.com>
2025-06-10 19:24:16 +0300

    Remove comments from scripts

--- bash_scripts/ssh_guard/ssh_guard.sh
+++ bash_scripts/ssh_guard/ssh_guard.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Log file path
 AUTH_LOG="/var/log/auth.log"
 
-# Threshold for failed attempts
 THRESHOLD=5
 
-# Time range (last 10 minutes)
 TIME_FRAME=$(date --date='10 minutes ago' '+%b %e %H:%M')
 
-# Get IPs with failed login attempts
 IPS=$(grep "Failed password" $AUTH_LOG | awk -v time="$TIME_FRAME" '$0 > time' | awk '{print $(NF-3)}' | sort | uniq -c | awk -v threshold="$THRESHOLD" '$1 >= threshold {print $2}')
 
 for IP in $IPS; do
